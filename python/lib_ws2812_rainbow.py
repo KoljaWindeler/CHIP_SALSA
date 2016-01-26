2a753984253e70e638504db5cde8d426a51157ea
KoljaWindeler <Kolja.Windeler@gmail.com>
2016-01-26 10:23:18 -0600

    fixed poor man rainbow to full 3*255 colors

--- python/lib_ws2812_rainbow.py
+++ python/lib_ws2812_rainbow.py
@@ -2,28 +2,25 @@ import time
 import arduino_bridge
 
 # set pin 11 as 20 ws2812, all with unique colors
-LC=18
-ROUNDS=10
-DELAY=0.02
+ledcount=18
+rounds=10
+delay=0.02
 pin = 11
 
 arduino = arduino_bridge.connection()
-arduino.setup_ws2812_common_color_output(pin,LC)
+arduino.setup_ws2812_common_color_output(pin,ledcount)
 
 # create a color array, rainbow for demonstration
 colorArray = []
-MAX=(255//LC)*LC
-STEP=MAX//LC
-for i in range(0,LC//3):
-	colorArray.append(arduino_bridge.Color(MAX-STEP*i, STEP*i, 0))
-for i in range(0,6):
-	colorArray.append(arduino_bridge.Color(0, MAX-STEP*i, STEP*i))
-for i in range(0,6):
-	colorArray.append(arduino_bridge.Color(STEP*i, 0, MAX-STEP*i))
+for i in range(0,256):
+	colorArray.append(arduino_bridge.Color(255-i, i, 0))
+for i in range(0,256):
+	colorArray.append(arduino_bridge.Color(0, 255-i, i))
+for i in range(0,256):
+	colorArray.append(arduino_bridge.Color(i, 0, 255-i))
 
 # send it
-a=0
-for ii in range(0,ROUNDS*255):
+for ii in range(0,rounds*3*255):
 	print("Round "+str(ii))
 	# set the current array
 	arduino.ws2812set(pin,colorArray[0])
@@ -35,4 +32,4 @@ for ii in range(0,ROUNDS*255):
 	colorArray[len(colorArray)-2]=temp
 		
 	# wait a little
-	time.sleep(DELAY)
+	time.sleep(delay)
