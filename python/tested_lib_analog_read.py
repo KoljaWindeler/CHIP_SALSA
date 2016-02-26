e9fa7967cc07551f3b25e0644e3347fd1d5ec88a
Kolja.Windeler@C.H.I.P <Kolja.Windeler@gmail.com>
2016-02-25 19:48:10 -0600

    mv files

--- python/tested_lib_analog_read.py
+++
@@ -1,9 +0,0 @@
-import arduino_bridge
-
-pin = 4
-
-arduino = arduino_bridge.connection()
-arduino.setup_analog_input(pin)
-analog_value = arduino.analogRead(pin)
-
-print("GPIO pin "+str(pin)+" read as "+str(analog_value)+" ~ "+str(analog_value*5.0/1024)+"V")
--- python/tested_lib_digital_read.py
+++
@@ -1,9 +0,0 @@
-import arduino_bridge
-
-pin = 8
-
-arduino = arduino_bridge.connection()
-arduino.setup_digital_input(pin)
-digital_value = arduino.digitalRead(pin)
-
-print("GPIO pin "+str(pin)+" read as "+str(digital_value)+" ~ "+str(digital_value*5.0)+"V")
